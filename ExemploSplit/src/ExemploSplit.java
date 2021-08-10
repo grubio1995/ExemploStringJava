e646d940e2ad2a7bbc7146519cf5e808fa585233
grubio1995 <gabriel_pinhatarubio@hotmail.com>
2021-08-10 15:14:49 -0300

    Exemplo utilizando split com strings

---
+++ ExemploSplit/src/ExemploSplit.java
@@ -0,0 +1,17 @@
+
+public class ExemploSplit {
+	
+	
+	public static void main(String[] args) {
+		
+		String s = "potato apple lemon orange";
+		
+		String[] vect = s.split(" ");
+		
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+		System.out.println(vect[3]);
+
+	}
+}
